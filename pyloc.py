cfc6e64935c5a3bf7d507a828bc7d948c417f5d8
James Booth <boothj5@gmail.com>
2011-09-03 21:16:35 +0100

    Tidy

--- pyloc.py
+++ pyloc.py
@@ -1,67 +1,30 @@
 #!/usr/bin/python
-
 import os
 from optparse import OptionParser
 
-# constants
-EXTENSIONS = "extensions"
-LINECOMMENTS = "linecomments"
-BLOCKSTART = "blockstart"
-BLOCKEND = "blockend"
-COUNT = "count"
-JAVA = "java"
-HASKELL = "haskell"
-PYTHON = "python"
-C = "c"
-
-# language definitions
-languages = { JAVA: 
-                { EXTENSIONS:   [ ".java" ] ,
-                  LINECOMMENTS: "//" ,
-                  BLOCKSTART:   "/*" ,
-                  BLOCKEND:     "*/" ,
-                  COUNT: 0 } ,
-              HASKELL:
-                { EXTENSIONS:   [ ".hs" , ".lhs" ] ,
-                  LINECOMMENTS: "--" ,
-                  BLOCKSTART:   "{-" ,
-                  BLOCKEND:     "-}"  ,
-                  COUNT: 0 } ,
-              C:
-                { EXTENSIONS:   [ ".c" , ".h" ] ,
-                  LINECOMMENTS: "//" ,
-                  BLOCKSTART:   "/*" ,
-                  BLOCKEND:     "*/" ,
-                  COUNT: 0 } ,
-              PYTHON:
-                { EXTENSIONS:   [ ".py" ] ,
-                  LINECOMMENTS: "#" ,
-                  COUNT: 0 } } 
+from languages import *
 
 # globals
-guessed_lang = False
-srcfiles = 0
-loc = 0
-paths = []
+in_comment = False
 
 # functions
-def is_source(filename):
+def is_source(filename, language, include_tests):
     result = False ;
     correct_ext = False ;
 
     for ext in languages[language][EXTENSIONS]:
         if filename.endswith(ext):
             correct_ext = True
-    is_test = "test" in filename or "Test" in filename
-    
+    is_test = "test" in filename.lower()
+
     if correct_ext:
         result = True
-    if not tests and is_test:
+    if not include_tests and is_test:
         result = False   
 
     return result
 
-def is_comment(line):
+def is_comment(line, language):
     global in_comment
     linecmnt = languages[language][LINECOMMENTS]
 
@@ -73,10 +36,10 @@ def is_comment(line):
             if line.strip().endswith(blkend):
                 in_comment = False
                 return True
-    
+
         if line.strip().startswith(blkstart) and line.strip().endswith(blkend):
             return True
-    
+
         if line.strip().startswith(blkstart):
             in_comment = True
             return True
@@ -89,15 +52,15 @@ def is_comment(line):
     
     return False
 
-def is_code(line):
-    if is_comment(line):
+def is_code(line, language):
+    if is_comment(line, language):
         return False
     elif line.strip() == "":
         return False
     else:
         return True
 
-def guess_lang():
+def guess_lang(directory):
     for dirname, dirnames, filenames in os.walk(directory):
         for filename in filenames:
             basename, extension = os.path.splitext(filename) ;
@@ -107,6 +70,7 @@ def guess_lang():
 
     result = None
     highest = 0
+
     for lang in languages:
         if languages[lang][COUNT] > highest:
             result = lang ;
@@ -114,70 +78,69 @@ def guess_lang():
     
     return result
 
-# handle arguments
-parser = OptionParser()
-parser.add_option("-d", "", dest="directory",
-                  help="Directory to search")
-parser.add_option("-l", "", dest="language",
-                  help="Source language (java, haskell, python, c), will guess if not specified")
-parser.add_option("-t", "",
-                  action="store_true", dest="tests", default=False,
-                  help="Include tests")
+def parse_opts():
+    guessed_lang = False 
+    parser = OptionParser()
+    parser.add_option("-d", "", dest="directory",
+                      help="Directory to search")
+    parser.add_option("-l", "", dest="language",
+                      help="Source language (java, haskell, python, c), will guess if not specified")
+    parser.add_option("-t", "",
+                      action="store_true", dest="tests", default=False,
+                      help="Include tests")
+    (options, args) = parser.parse_args()
 
-(options, args) = parser.parse_args()
+    if not options.directory:
+        parser.error("You must specify at least a directory, try pyloc.py -h")
+    directory = options.directory
 
-if not options.directory:
-    parser.error("You must specify at least a directory, try pyloc.py -h")
+    if options.language:
+        language = options.language
+    else:
+        language = guess_lang(directory)
+        guessed_lang = True
 
-directory = options.directory
-if options.language:
-    language = options.language
-else:
-    language = guess_lang()
-    guessed_lang = True
-tests = options.tests
+    include_tests = options.tests
 
-# main script
+    return (directory, language, guessed_lang, include_tests)
 
-print
-print "PYLOC"
-print "-----"
-print
-
-in_comment = False
-
-if language == None:
-    print "Could not find any code!"
-else:
-
-    print "Folder   : " + directory
-    guessed = "" ;
-
-    if guessed_lang:
-        guessed = " (guessed)"
-
-    print "Language : " + language + guessed
-
-    print "Tests    : " + str(tests)
-    print
-
-    for lang in languages:
-        languages[lang][COUNT] = 0
-
-    for dirname, dirnames, filenames in os.walk(directory):
-        for filename in filenames:
-            if is_source(filename):
-                languages[language][COUNT] = languages[language][COUNT] + 1
-                paths.append(dirname + "/" + filename)
-
-    for path in paths:
-        f = open(path)
-        for line in f:
-            if is_code(line):
-                loc = loc + 1         
+def main():
+    global in_comment
+    loc = 0 
+    paths = []
+    (directory, language, guessed_lang, include_tests) = parse_opts()
+    in_comment = False
 
     print
-    print "Source files  : " + str(languages[language][COUNT])
-    print "Lines of code : " + str(loc)
+    print "PYLOC"
+    print "-----"
+    print
+    if language == None:
+        print "Could not find any code!"
+    else:
+        for lang in languages:
+            languages[lang][COUNT] = 0
 
-print
+        for dirname, dirnames, filenames in os.walk(directory):
+            for filename in filenames:
+                if is_source(filename, language, include_tests):
+                    languages[language][COUNT] = languages[language][COUNT] + 1
+                    paths.append(dirname + "/" + filename)
+
+        for path in paths:
+            f = open(path)
+            for line in f:
+                if is_code(line, language):
+                    loc = loc + 1         
+
+        print "Folder   : " + directory
+        print "Language : " + language + (lambda g : " (guessed)" if g else "")(guessed_lang)
+        print "Tests    : " + str(include_tests)
+        print
+        print
+        print "Source files  : " + str(languages[language][COUNT])
+        print "Lines of code : " + str(loc)
+    print
+
+if __name__ == "__main__":
+    main()
