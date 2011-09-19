86d0a63fb556a46e9d6d8f7965f4ce77d882c697
James Booth <boothj5@gmail.com>
2011-09-19 23:28:19 +0100

    Handle cancel on dir browser

--- pyloc.py
+++ pyloc.py
@@ -40,73 +40,76 @@ class MyFrame(wx.Frame):
     def on_open_dir(self, event):
         dirname = ""
         dialog = wx.DirDialog(self, "Choose a directory")
-        if dialog.ShowModal() == wx.ID_OK:
+        action = dialog.ShowModal() 
+        if action == wx.ID_OK:
             dirname = dialog.GetPath()
-        dialog.Destroy()
-        self.sizer.DeleteWindows()
-        lang_stats = {}
+            dialog.Destroy()
+            self.sizer.DeleteWindows()
+            lang_stats = {}
         
-        pylocstats.init_stats(dirname, lang_stats)
+            pylocstats.init_stats(dirname, lang_stats)
 
-        text = "\n" + "PYLOC\n" + "-----\n"
-        text = text + "Folder   : " + dirname + "\n\n"
-        if not lang_stats:
-            text = text + "Could not find any code!\n"
-        else:
-            text = text + pylocstats.show_summary(lang_stats)
-            text = text + pylocstats.show_lang_stats(lang_stats)
+            text = "\n" + "PYLOC\n" + "-----\n"
+            text = text + "Folder   : " + dirname + "\n\n"
+            if not lang_stats:
+                text = text + "Could not find any code!\n"
+            else:
+                text = text + pylocstats.show_summary(lang_stats)
+                text = text + pylocstats.show_lang_stats(lang_stats)
         
-        self.stats = wx.TextCtrl(self, style=wx.TE_MULTILINE, size=(400,600))
-        self.stats.WriteText(text)
-        self.stats.SetEditable(False)
+            self.stats = wx.TextCtrl(self, style=wx.TE_MULTILINE, size=(400,600))
+            self.stats.WriteText(text)
+            self.stats.SetEditable(False)
 
-        self.langpie = PieCtrl(self, -1, wx.DefaultPosition, size=(400,600))
-        self.langpie.SetAngle(radians(25))
-        self.langpie.GetLegend().SetTransparent(True)
-        self.langpie.GetLegend().SetHorizontalBorder(10)
-        self.langpie.GetLegend().SetWindowStyle(wx.STATIC_BORDER)
-        self.langpie.GetLegend().SetLabelFont(wx.Font(10, wx.FONTFAMILY_DEFAULT,
-                                                   wx.FONTSTYLE_NORMAL,
-                                                   wx.FONTWEIGHT_NORMAL,
-                                                   False, "Courier New"))
-        self.langpie.GetLegend().SetLabelColour(wx.Colour(0, 0, 127))
+            self.langpie = PieCtrl(self, -1, wx.DefaultPosition, size=(400,600))
+            self.langpie.SetAngle(radians(25))
+            self.langpie.GetLegend().SetTransparent(True)
+            self.langpie.GetLegend().SetHorizontalBorder(10)
+            self.langpie.GetLegend().SetWindowStyle(wx.STATIC_BORDER)
+            self.langpie.GetLegend().SetLabelFont(wx.Font(10, wx.FONTFAMILY_DEFAULT,
+                                                       wx.FONTSTYLE_NORMAL,
+                                                       wx.FONTWEIGHT_NORMAL,
+                                                       False, "Courier New"))
+            self.langpie.GetLegend().SetLabelColour(wx.Colour(0, 0, 127))
 
-        colours = [ wx.Colour(200, 50, 50) ,
-                    wx.Colour(50, 200, 50) ,
-                    wx.Colour(50, 50, 200) ,
-                    wx.Colour(100, 0, 200) ,
-                    wx.Colour(200, 200, 0) ,
-                    wx.Colour(0, 0, 200) ,
-                    wx.Colour(200, 0, 200) ,
-                    wx.Colour(0, 200, 200) ,
-                    wx.Colour(0, 0, 50) ,
-                    wx.Colour(0, 50, 0) ]
+            colours = [ wx.Colour(200, 50, 50) ,
+                        wx.Colour(50, 200, 50) ,
+                        wx.Colour(50, 50, 200) ,
+                        wx.Colour(100, 0, 200) ,
+                        wx.Colour(200, 200, 0) ,
+                        wx.Colour(0, 0, 200) ,
+                        wx.Colour(200, 0, 200) ,
+                        wx.Colour(0, 200, 200) ,
+                        wx.Colour(0, 0, 50) ,
+                        wx.Colour(0, 50, 0) ]
         
-        colour = 0
-        counts = []
-        for lang in lang_stats:
-            name = lang
-            total = lang_stats[lang][pylocstats.TOTAL_LINES]
-            counts.append((name, total))
+            colour = 0
+            counts = []
+            for lang in lang_stats:
+                name = lang
+                total = lang_stats[lang][pylocstats.TOTAL_LINES]
+                counts.append((name, total))
 
-        sorted_counts = reversed(sorted(counts, key=lambda l: l[1]))
+            sorted_counts = reversed(sorted(counts, key=lambda l: l[1]))
 
-        for lang, count in sorted_counts:
-            part = PiePart()
+            for lang, count in sorted_counts:
+                part = PiePart()
     
-            lines = pylocstats.format_thousands(count)
-            part.SetLabel(lang + " (" + str(lines) + ")")
-            part.SetValue(count)
-            part.SetColour(colours[colour])
-            colour = colour + 1
-            self.langpie._series.append(part)
+                lines = pylocstats.format_thousands(count)
+                part.SetLabel(lang + " (" + str(lines) + ")")
+                part.SetValue(count)
+                part.SetColour(colours[colour])
+                colour = colour + 1
+                self.langpie._series.append(part)
         
-        self.sizer.Add(self.langpie, 1, wx.EXPAND | wx.ALIGN_LEFT)
-        self.sizer.Add(self.stats, 1, wx.EXPAND | wx.ALIGN_RIGHT)
-        self.SetSizer(self.sizer)
-        self.SetAutoLayout(1) 
-        self.sizer.Fit(self)
-        self.Layout()
+            self.sizer.Add(self.langpie, 1, wx.EXPAND | wx.ALIGN_LEFT)
+            self.sizer.Add(self.stats, 1, wx.EXPAND | wx.ALIGN_RIGHT)
+            self.SetSizer(self.sizer)
+            self.SetAutoLayout(1) 
+            self.sizer.Fit(self)
+            self.Layout()
+        else:
+            dialog.Destroy()
 
 def main():
     locale.setlocale(locale.LC_ALL, '')
