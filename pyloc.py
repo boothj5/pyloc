815322ebbb1f094a6ce841aaf902a3b5f508fdab
James Booth <boothj5@gmail.com>
2012-02-20 20:16:47 +0000

    Added GPL

--- pyloc.py
+++ pyloc.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python
+# 
+# pyloc.py
+#
+# Copyright (C) 2012 James Booth <boothj5@gmail.com>
+# 
+# This file is part of Pyloc.
+#
+# Pyloc is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Pyloc is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pyloc.  If not, see <http://www.gnu.org/licenses/>.
+
 from languages import *
 import wx
 from langctrl import LangPieCtrl, LangStatsCtrl, StatsProgressDialog
